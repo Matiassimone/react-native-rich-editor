97857f611eb136622d7e27d24781f9d94cf7f167
Matias Simone <matiassimone.d@gmail.com>
2021-04-22 17:25:09 -0300

    Add markdown rules

--- src/editor.js
+++ src/editor.js
@@ -430,7 +430,24 @@ function createHTML(options = {}) {
             content.autocomplete = 'off';
             content.className = "pell-content";
             content.oninput = function (_ref) {
-                // var firstChild = _ref.target.firstChild;
+
+                // Rules Markdown Regex expressions.
+                let numberedListRegex = /^[0-9]+[)]+\&nbsp;$/
+                let bulletListRegex = /^[*|-]+\&nbsp;$/
+
+                let lastedChild = _ref.target.lastChild;
+                while (lastedChild.hasChildNodes() && lastedChild.lastChild.innerHTML) {
+                  lastedChild = lastedChild.lastChild;
+                }
+                let lastedChildInnerContent = lastedChild.innerHTML;
+
+                if(bulletListRegex.test(lastedChildInnerContent)) {
+                  Actions['unorderedList'].result();
+                }
+                if(numberedListRegex.test(lastedChildInnerContent)) {
+                  Actions['orderedList'].result();
+                }
+
                 if ((anchorNode === void 0 || anchorNode === content) && queryCommandValue(formatBlock) === ''){
                     formatParagraph(true);
                 } else if (content.innerHTML === '<br>') content.innerHTML = '';
